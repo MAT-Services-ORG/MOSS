a1ca755c2dbd2b74431a54e458d6eaacbb1a5df6
MAT Services Administrator <157519566+MAT-services@users.noreply.github.com>
2026-06-24 12:54:38 +0000

    Now with "installer" UI

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,11 +3,24 @@
 
 #!/bin/bash
 
-# Only for keyboard changes.
-# dpkg-reconfigure keyboard-configuration
-# setupcon
-
 apt update
 apt remove snapd
 apt install wayland-protocols xwayland # GUI
 apt install kwin_wayland # Wayland compositor
+apt install libwaykitgtk-6.0-4 # WebView
+
+read -p "Change keyboard method ? [Y/n] " -n 1 -r # Only for keyboard changes.
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    dpkg-reconfigure keyboard-configuration
+    setupcon
+fi
+
+read -p "Install Dev Tools ? [Y/n] " -n 1 -r # Developper Only. Check https://tauri.app/reference/cli/ for references.
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    apt install npm
+    npm install --save-dev @tauri-apps/cli@latest
+fi
